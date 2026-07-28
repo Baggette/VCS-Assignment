c037221ab711a06698d2ed2ddbb6a0593d6f8cc2
Baggette <rishijsarwan@gmail.com>
2026-07-28 14:33:55 -0500

    Add warning message for issues found in logs resolves #5

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -3,4 +3,6 @@ print("found logs...")
 #Malciouscode here 
 print("accessing auth server over ftp and http...")
 print("Encrypting logs with base64...")
+#warning for issues found in logs resolves issue #5
+print("Found Issues ssh server, currently root ssh is enabled with the default password of \"basil123\"" )
 input("done!...")
