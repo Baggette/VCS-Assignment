3af9278bdf4a3a417e4cf0dd9d0720b833443477
Baggette <rishijsarwan@gmail.com>
2026-07-28 14:28:20 -0500

    Add LogAnalyzer.py with initial log processing functionality

---
+++ LogAnalyzer.py
@@ -0,0 +1,6 @@
+print("checking logs...")
+print("found logs...")
+#Malciouscode here 
+print("accessing auth server over ftp and http...")
+print("Encrypting logs with base64...")
+input("done!...")
