1d607287cf8271cd5522e8ae1f3278f8cbca7c70
Baggette <rishijsarwan@gmail.com>
2026-07-28 15:04:10 -0500

    Update LogAnalyzer.py to change log encryption method from base64 to sha256 for improved security #7

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -2,7 +2,7 @@ print("checking logs...")
 print("found logs...")
 #Malciouscode here 
 print("accessing auth server over sftp and https...")#Now uses sftp and https instead to be more secure fixes #6
-print("Encrypting logs with base64...")
+print("Encrypting logs with sha256...")#resolved issue #7
 #warning for issues found in logs resolves issue #5
 print("Found Issues ssh server, currently root ssh is enabled with the default password of \"basil123\"" )
 input("done!...")
